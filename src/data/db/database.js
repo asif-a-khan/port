2e65b4349cb58da2d723a5e59f48101f6255b865
Asif <asif14.k@hotmail.com>
2021-03-09 14:36:22 -0500

    Fixed size for slack

--- src/data/db/database.js
+++ src/data/db/database.js
@@ -168,7 +168,7 @@ const database = {
       id: 6,
       title: "Pendrive",
       subtitle: "Free pendrive mockup design.",
-      imageUrl: "/images/slack.png",
+      imageUrl: "/images/slack.jpg",
       largeImageUrl: ["/images/portfolio-image-6-lg.jpg"],
       url: 'https://codedslack.netlify.app/',
       github: 'https://github.com/asif-a-khan/codedSlack'
